51e436b8a37e74bd7183ace00df25ab3f24e6468
Agryo Araujo <agryostallion@gmail.com>
2023-08-27 16:25:36 -0300

    Adicionando comentários explicativos

--- src/stream_api/Desafio01.java
+++ src/stream_api/Desafio01.java
@@ -8,7 +8,27 @@ public class Desafio01 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        List<Integer> ordenados =  numeros.stream().sorted().collect(Collectors.toList());
+        List<Integer> ordenados = numeros.stream() //O método "Stream", transforma a lista "numeros" em uma stream.
+                /* 
+                 * O método "Sorted" abaixo, ordena a lista que agora é um Stream.
+                 * Se for "Integer" é ordem crescente, "String" em ordem alfabética, segue a ordem natural.
+                 * Ele também pode receber uma ordenação personalizada como um outro método dentro dos parenteses.
+                 * Essa ordenação personalizada é um Comparator. "Ex. sorted(Comparator<T> comparator)"
+                 * Ex.: ".sorted(Comparator.comparing(Person::getAge))"
+                 * Nesse exemplo o método "sorted()" com um comparador criado por "Comparator.comparing()" que se 
+                 * baseia no método "getAge()" da classe "Person".
+                 * Isso ordenará as pessoas com base em suas idades, do mais jovem ao mais velho.
+                 */
+                .sorted()
+                /*
+                 * O método "collect" abaixo, coleta todos os dados do "Stream" e pode transformar em um outro tipo.
+                 * Como uma lista, um conjunto, um mapa ou até mesmo realizar uma operação personalizada de agregação.
+                 * Na chamada abaixo, ele apenas coleta os dados do "Stream" para uma "List".
+                 * Coleta com Filtros e Transformações: Você pode combinar o "collect()" com outras operações intermediárias, 
+                 * como "filter()" e "map()", para coletar apenas elementos que atendem a certos critérios ou transformar 
+                 * os elementos antes da coleta.
+                 */
+                .collect(Collectors.toList());
 
         System.out.println(ordenados);
     }
--- src/stream_api/Desafio02.java
+++ src/stream_api/Desafio02.java
@@ -7,7 +7,39 @@ public class Desafio02 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        int somaPares = numeros.stream().filter(n -> n % 2 == 0).mapToInt(Integer::intValue).sum();
+        int somaPares = numeros.stream()
+            /*
+             * O método "filter()" é usado para filtrar os elementos do "Stream" com base em uma 
+             * condição especificada. Ele retorna um novo Stream contendo apenas os elementos que 
+             * atendem à condição. No método abaixo, ele filtra os números e verifica se o resultado 
+             * do número dividido por dois é igual a zero. (Verifica se é par)
+             * Pode ser feito um novo "filter()" encadeado para filtar mais.
+             * (Ex.: ".filter(n -> n > 5)") Nesse exemplo ele verifica os números pares maiores que cinco.
+             * E assim sucessivamente caso precise.
+             */
+            .filter(n -> n % 2 == 0)
+            /*
+             * O método "mapToInt()" é uma operação intermediária em Streams do Java que é usada para transformar
+             * os elementos de um Stream em valores inteiros. Serve para mapear os elementos para tipos primitivos "int"
+             * em vez de objetos. A principal vantagem de usar "mapToInt()" é que ele retorna um "IntStream", que é uma 
+             * subinterface de Stream otimizada para lidar com valores inteiros primitivos. Resulta em melhor desempenho 
+             * e economia de memória em relaçao ao "map()".
+             * No método abaixo, ele está transformando os elementos em valores inteiros primitivos "int". (".mapToInt()")
+             * Em seguia dentro dos parênteses ele faz referência a classe "Integer" (da List la em cima) e usa o método
+             * "intValue" da classe "Integer" para pegar os valores inteiros dessa "List" com o nome "numeros".
+             */
+            .mapToInt(Integer::intValue)
+            /*
+             * O método "sum()" é uma operação terminal disponível em Streams do Java que é usada para calcular a soma dos 
+             * elementos de um Stream de valores numéricos. Essa operação é bastante útil quando você deseja obter a soma 
+             * total dos valores contidos no Stream.
+             * O método sum() só está disponível em Streams de tipos numéricos, como "IntStream", "LongStream" e "DoubleStream".
+             * Ele retorna o resultado da soma como um valor do mesmo tipo numérico do Stream. 
+             * Se o Stream estiver vazio, o "sum()" retornará "0".
+             * O sum() é uma operação de redução, o que significa que é uma operação terminal que combina os elementos do 
+             * Stream em um único resultado.
+             */
+            .sum();
 
         System.out.println(somaPares);
     }
