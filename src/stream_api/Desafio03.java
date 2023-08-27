64b2e44a436085d794033757e9c6d25c78f04f01
Agryo Araujo <agryostallion@gmail.com>
2023-08-27 02:54:06 -0300

    Resolução do Desafio 3

---
+++ src/stream_api/Desafio03.java
@@ -0,0 +1,17 @@
+package stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio03 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean todosPositivos = numeros.stream().allMatch(n -> n > 0);
+
+        if (todosPositivos)
+            System.out.println("Todos os números são positivos.");
+        else
+            System.out.println("Existem números negativos!");
+    }
+}
