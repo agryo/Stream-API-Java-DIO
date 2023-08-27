3ce98ba7e9b1057ef32851bb7cdda6e854e5e5e3
Agryo Araujo <agryostallion@gmail.com>
2023-08-27 00:30:59 -0300

    Atualização

--- src/functional_interface/examples/ConsumerExample.java
+++ src/functional_interface/examples/ConsumerExample.java
@@ -2,7 +2,7 @@ package functional_interface.examples;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
+//import java.util.function.Consumer;
 
 /**
  * Representa uma operação que aceita um argumento do tipo T e não retorna nenhum resultado.
@@ -15,15 +15,13 @@ public class ConsumerExample {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
         // Usar o Consumer com expressão lambda para imprimir números pares
-        Consumer<Integer> imprimirNumeroPar = numero -> {
+/*        Consumer<Integer> imprimirNumeroPar = numero -> {
             if (numero % 2 == 0) {
                 System.out.println(numero);
             }
         };
-
+ */
         // Usar o Consumer para imprimir números pares no Stream
-        numeros.stream()
-                .filter(n -> n % 2 == 0)
-                .forEach(System.out::println);
+        numeros.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
     }
 }
